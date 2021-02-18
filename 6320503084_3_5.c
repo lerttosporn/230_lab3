0d5b180774f644661c55de4721d59dd66d071fa6
lerttosprorn <lerttosporn.s@ku.th>
2021-02-18 17:49:08 +0700

    Compilation completed

---
+++ 6320503084_3_5.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,j=2,ar[3],i=0,ln;
+    scanf("%d",&n);
+    ln=n;
+    while(1)
+    {
+        if(n%j==0)
+        {
+            n=n/j;
+            ar[i]=j;
+            i++;
+        }
+        else if(n==1)
+        {
+            break;
+        }
+            j++;
+    }
+
+        if(ar[0]==ar[1]||ar[0]==ar[2]||ar[1]==ar[2])
+    {
+          printf("%d a not Lucky Number.",ln);
+    }
+    else
+    {
+        printf("%d is a Lucky Number.",ln);
+    }
+
+    return 0;
+}
+
