c5116d0a2249ea788f6a1ba4f05c637345a64263
lerttosprorn <lerttosporn.s@ku.th>
2021-02-18 16:19:01 +0700

    fac

--- 6320503084_3_3.c
+++ 6320503084_3_3.c
@@ -5,20 +5,19 @@ int main()
     scanf("%d",&n);
     while(1)
     {
-            if(n%j==0)
-            {
-                printf("%d",j);
-                n=n/j;
-            }
-            else
-            {
-                j++;
-            }
+        if(n%j==0)
+        {
+            printf("%d\n",j);
+            n=n/j;
+        }
+        else
+        {
+            j++;
+        }
         if(n==0)
-        {printf("%d",n);
+        {
             break;
         }
-        printf("\n");
     }
 
     return 0;
